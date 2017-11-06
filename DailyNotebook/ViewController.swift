253e584e3ca0bf56c63bba9fc4f6335fb9afb6e3
LiamKirkland <liam.b.kirkland@gmail.com>
2017-11-06 08:36:21 -0500

    Started tableView code

--- DailyNotebook/ViewController.swift
+++ DailyNotebook/ViewController.swift
@@ -8,7 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    var eventNames: Array = ["Example Event"]
+    var eventTimes: Array = [""]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return eventNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var myCell = tableView.dequeueReusableCell(withIdentifier: "myCell")
+        if (myCell == nil)
+        {
+            myCell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "myCell")
+        }
+        myCell?.textLabel?.text = eventNames[indexPath.row]
+        myCell?.detailTextLabel?.text = eventTimes[indexPath.row]
+        
+        return myCell!
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
